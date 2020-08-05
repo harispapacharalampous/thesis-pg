5ffacd8b1d8cf0dcfe3e5cdc8c699b851cf8b40c
haris <harispapacharalampous@gmail.com>
2020-08-05 12:56:16 +0300

    js update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,25 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require Chart.bundle
+//= require chartkick
+//= require jsapi
+//= require jquery
+//= require popper
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
+//= require materialize-sprockets
+
+
+// Loads all Semantic javascripts
+//= require semantic-ui
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("semantic-ui-sass")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
