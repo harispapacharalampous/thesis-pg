e24adff423e082ac1925ca2cf1a836d16af9f75b
haris <harispapacharalampous@gmail.com>
2020-08-06 17:40:50 +0300

    Revert "consider_all_requests_local = true"
    This reverts commit 423fe5a729176dc99469216e0fdce3c172888bcf.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,5 +129,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-    config.action_controller.consider_all_requests_local = true
 end
